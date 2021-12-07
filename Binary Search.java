ec411b9d9087d82a0f703e6cfc447a408e09dfb7
Yashvardhan <yashsingh121@gmail.com>
2021-12-07 11:49:43 +0530

    Create  Binary Search.java

---
+++ Binary Search.java
@@ -0,0 +1,17 @@
+class Solution {
+    int binarysearch(int arr[], int n, int k){
+        int low =0;
+        int high = arr.length -1;
+        while(low<=high){
+            int mid =low+ (high-low)/2;
+            if(arr[mid]==k){
+                return mid;
+            }else if(arr[mid]<k){
+                low= mid+1;
+            }else{
+                high= mid-1;
+            }
+        }
+        return -1;
+    }
+}
